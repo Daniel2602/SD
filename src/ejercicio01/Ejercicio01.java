ff1886591ac033b86a03138f92d66ca91b8397d2
Daniel <Daniel@Daniel-PC>
2018-04-04 21:30:26 -0500

    Commit inicial Se borraron los comentarios

--- src/ejercicio01/Ejercicio01.java
+++ src/ejercicio01/Ejercicio01.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio01;
 
-/**
- *
- * @author Daniel
- */
+
 public class Ejercicio01 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numero;
+        numero = 1;
+        System.out.println("ejercicio01.Ejercicio01.main()");
     }
     
 }
